ab84be52941469555ce6589514cc8e89d5650416
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-05-17 09:13:15 -0300

    primeros ejercicios

---
+++ js/tp1.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+
+alert("Un mensaje");
+
+//Ejercicio 2
+
+document.write("Hello World<br>");
+
+//Ejercicio 3
+
+document.write("<br>La suma de 2 + 5 es igual a " + (2 + 5));
+
+//Ejercicio 4
+
+let username = prompt("Ingrese su nombre de usuario: ");
+document.write("<br><br>Hola " + username);
+
+//Ejercicio 5
+
+let numero1 = parseInt(prompt("Escribe un numero: "));
+let numero2 = parseInt(prompt("Escribe otro numero: "));
+document.write("El resultado de la suma es: " + (numero1 + numero2));
