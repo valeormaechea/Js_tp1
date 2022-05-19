a620a059d97c387523fdebf44e727958ac8ea440
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-05-19 09:38:18 -0300

    agrego ejercicio8

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Ingrese un numero: "));
+
+if (numero % 2 === 0) {
+  document.write("El numero es divisible por 2.");
+} else {
+  document.write("El numero no es divisible por 2.");
+}
