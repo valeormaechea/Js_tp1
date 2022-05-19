42a918434f5a0096498d4fcd70ee65a369435b30
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-05-19 08:29:18 -0300

    agrego ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -2,9 +2,9 @@ let numero1 = parseInt(prompt("Ingrese el primer numero: "));
 let numero2 = parseInt(prompt("Ingrese el segundo numero: "));
 
 if (numero1 > numero2) {
-  document.write("El numero " + numero1 + " es mas grande.");
+  document.write("El numero " + numero1 + " es el mas grande.");
 } else if (numero2 > numero1) {
-  document.write("El numero " + numero2 + " es mas grande.");
+  document.write("El numero " + numero2 + " es el mas grande.");
 } else {
   document.write("Los numeros son iguales.");
 }
